5ac38997ef197a269f5e7acf1ab7e2d25970ae10
Marc de Fluiter <marc@famdefluiter.nl>
2022-03-14 13:34:22 +0100

    Added example document and support for multiple items in list

--- src/LatexGenerator.hs
+++ src/LatexGenerator.hs
@@ -1,5 +1,6 @@
-module LatexGenerator where
+module LatexGenerator (documentToLaTeX) where
 
+-- Definition of document and such temporarily here
 type Document = [RootExpr]
 
 data RootExpr = Heading Int Expr
@@ -14,9 +15,21 @@ data Expr = Sequence [Expr]
           | OrderedList Expr
           | UnorderedList Expr
 
+-- example document for testing
+exampleDocument :: Document
+exampleDocument = [Heading 0 head1, Body body1, Heading 1 head1_1, Body body1_1, Heading 0 head2,  Body body2]
+    where
+        head1 = Bold (Text "Heading 1")
+        body1 = OrderedList (Italic (Text "Text in list"))
+        head1_1 = Italic (Text "Heading 1.1")
+        body1_1 = UnorderedList (Sequence [Text "Text in list 1", Text "Text in list 2"])
+        head2 = Sequence [Text "This is ", Italic (Text "Heading 2")]
+        body2 = Sequence [Text "Link to Google:", Hyperlink "https://www.google.com/" (Text "Google")]
+
 
 documentToLaTeX :: Document -> String
 documentToLaTeX es = "\\documentclass[12pt]{article}\n"
+                  ++ "\\usepackage{hyperref}\n"
                   ++ "\\begin{document}\n" 
                   ++ concatMap rootExprToLaTeX es
                   ++ "\\end{document}\n"
@@ -38,9 +51,17 @@ exprToLaTeX (Image s e)       = "\\begin{figure}\n"
                              ++ "\\includegraphics{" ++ s ++ "}\n"
                              ++ "\\caption{" ++ exprToLaTeX e ++ "}\n" 
                              ++ "\\end{figure}\n"
+
+exprToLaTeX (OrderedList   (Sequence es)) = "\\begin{enumerate}\n" 
+                                         ++ concatMap (\e -> "\\item " ++ exprToLaTeX e ++ "\n") es
+                                         ++ "\\end{enumerate}\n"
+exprToLaTeX (UnorderedList (Sequence es)) = "\\begin{itemize}\n"
+                                         ++ concatMap (\e -> "\\item " ++ exprToLaTeX e ++ "\n") es
+                                         ++ "\\end{itemize}\n"
+
 exprToLaTeX (OrderedList e)   = "\\begin{enumerate}\n" 
-                             ++ exprToLaTeX e ++ "\n"
+                             ++ "\\item " ++ exprToLaTeX e ++ "\n"
                              ++ "\\end{enumerate}\n"
 exprToLaTeX (UnorderedList e) = "\\begin{itemize}\n"
-                             ++ exprToLaTeX e ++ "\n"
+                             ++ "\\item " ++ exprToLaTeX e ++ "\n"
                              ++ "\\end{itemize}\n"
