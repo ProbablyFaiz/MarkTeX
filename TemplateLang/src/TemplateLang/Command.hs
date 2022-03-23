5dceab30e5e61a3e5d0e1b65f3629f446bfd1431
Rick van de Bovenkamp <r.vandebovenkamp@students.uu.nl>
2022-03-23 16:00:22 +0100

    Document settings, haskell imports and lookup dot-notation

--- TemplateLang/src/TemplateLang/Command.hs
+++ TemplateLang/src/TemplateLang/Command.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
 module TemplateLang.Command where
 
 import TemplateLang.Values
 import TemplateLang.Boolean
 
+import Data.Bifunctor
 import Data.Data (Typeable)
+import qualified Data.Map as M
 import qualified Prelude as P
 import Prelude hiding (not)
 
-data MetaCommand = If Bool | IfVar String | Insert TValue | InsertVar String
+data MetaCommand = If Bool | IfVar String | 
+  Insert TValue | InsertVar String | 
+  DocSetting String TValue | DocSettings TData |
+  -- ImportQ = ModuleName QualifiedName
+  LoadHsFile String | Import String | ImportQ String String
   deriving (Read, Show, Typeable)
 
 tIf :: (ToBool a) => a -> MetaCommand
@@ -15,3 +22,9 @@ tIf = If . toBool
 
 tIfNot :: (ToBool a) => a -> MetaCommand
 tIfNot = If . not
+
+tDocSetting :: ToTValue a => String -> a -> MetaCommand
+tDocSetting str = DocSetting str . toTValue
+
+tDocSettings :: ToTValue a => [(String, a)] -> MetaCommand
+tDocSettings tdata = DocSettings $ M.fromList $ map (second toTValue) tdata
